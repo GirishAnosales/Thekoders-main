a469e31763a308c84182c49c8ee1b1c3b066fbd6
Morish-Anosales <anosales207@gmail.com>
2024-02-14 16:01:37 +0530

    Google tag manager and Google Analytics code add in index.html and app.compoment.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag:any;
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'THEKODERS';
+
+  constructor(private router: Router){
+		// const navEndEvents = router.events.pipe(
+		//   filter(event => event instanceof NavigationEnd),
+		// );
+		// navEndEvents.subscribe((event: NavigationEnd) => {
+		//   gtag('config', 'AW-11454875150', {
+		//     'page-path': event.urlAfterRedirects
+		//   });
+		// })
+	
+		  const routeChange = router.events
+			.pipe(
+			  filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+			)
+			.subscribe(event => {
+			  gtag('config', 'G-ZCXNK43YCN', {
+				'page-path': event.urlAfterRedirects
+			  })
+			});
+		
+	  }
 
   onClickScrollToTop() {
     window.scrollTo(0, 0);
